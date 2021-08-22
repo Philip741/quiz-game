6c639dfd541cb61b83574ae5bd5a0667b79ef59c
Philip Browning <philip741@gmail.com>
2021-08-22 10:29:26 -0400

    First commit

---
+++ assets/functions.js
@@ -0,0 +1,36 @@
+
+
+function clearContent() {
+    // clear gameForm 
+}
+
+function populateQuestion () {
+    //function that populates the question to the screen
+    // present choices as a list
+
+}
+
+function timer() {
+    // function that runs the game timer
+    //if answer is wrong subtract from the time
+}
+
+function checkAnswer () {
+    // function to check the answer to the question and return right or wrong
+}
+
+
+function gameOver() {
+    // function that runs when the timer hits zero
+}
+
+function storeScore () {
+    // function that saves initials and score
+}
+
+function runGame() {
+    // load on start button click
+    return true
+}
+
+
