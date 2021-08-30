d03f341d6e40d68abc0a6947d18bdb4fc8894746
Philip Browning <philip741@gmail.com>
2021-08-30 11:13:03 -0400

    Modified the init function to better work with the async of fetching the json file

--- assets/script.js
+++ assets/script.js
@@ -2,17 +2,88 @@ import * as gameFunc from './functions.js';
 
 var timerElement = document.querySelector("#timer-count");
 var startButton = document.querySelector("#quizStart");
-let questions = {
-    q0:"What two values can a boolean be set as ?",
-    q0Ans: "true of false",
-    q1:"What does DOM stand for ?"
-}
+var score = 0;
+var timerCount = 30;
+var qNumber = 'q1';
+var isWin = false;
 
 function init () {
-    console.log("started init");
-    gameFunc.timer(timerElement)
-    gameFunc.clearContent(gameForm);
-    gameFunc.setQuestion('gameForm', questions.q0, questions.q0Ans)
+    let currentQuestion = 1;
+    var gameFormEl = document.getElementById('gameForm');
+    let timer = setInterval(() => {
+        timerCount--;
+        timerElement.textContent = timerCount;
+        if (timerCount === 0) {
+           clearInterval(timer);
+           timerElement.textContent = timerCount;
+           gameFunc.gameOver(gameFormEl);
+        }
+        if (timerCount > 0 && isWin) {
+           clearInterval(timer)
+           timerCount = 0;
+           timerElement.textContent = timerCount;
+           gameFunc.gameOver(gameFormEl);
+        }
+    }, 1000);
+    //initial clear of game form    
+    gameFunc.clearContent(gameFormEl);
+
+    gameFunc.getQuestions()
+    .then(function(response) {
+        console.log(response);
+        gameFunc.newSetQuestion(gameFormEl,response,qNumber);
+        return response
+    })
+    .then(function(response){
+        gameFormEl.addEventListener('click', event => {
+            let qAns = qNumber + "Answer";
+            let answer = response[qAns];
+            let maxQuestions = response["numberQuestions"];
+            console.log(maxQuestions);
+            var answerContent = event.target.textContent;
+            //increment question number 
+            currentQuestion++
+            //correct answer
+            if (answer === answerContent) {
+                console.log("Answer Correct");
+                gameFunc.clearContent(gameFormEl);
+                //increment current question number
+                score++
+                console.log("Current question number: " + currentQuestion);
+                console.log("Current score: " + score);
+                qNumber = "q" + currentQuestion
+                if (currentQuestion > maxQuestions) {
+                    isWin = true;
+                    gameFunc.gameOver(gameFormEl);
+                }
+                else {
+                    gameFunc.newSetQuestion(gameFormEl,response,qNumber);
+                }
+            }
+            else if (answer !== answerContent) {
+                console.log("wrong answer!")
+                if (timerCount > 0) {
+                    let subtractAmt = timerCount - 5
+                    if (subtractAmt < 0) {
+                        isWin = true;
+                    }
+                    else {
+                        timerCount -= 5;
+                    }
+                timerElement.textContent = timerCount;
+                }
+            }
+            var myTarget = event.target;
+            myTarget.dataset.id = "Correct";
+
+        })
+    })
+    
 }
-console.log(startButton);
+
 startButton.addEventListener('click', init);
+
+
+
+
+
